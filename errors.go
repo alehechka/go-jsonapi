35e65877e9f9975c252c2468d3524a798159949e
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-04-27 15:51:18 -0500

    omit from internalError as well

--- errors.go
+++ errors.go
@@ -21,9 +21,9 @@ type Error struct {
 type internalError struct {
 	ID     string      `json:"id,omitempty"`
 	Links  LinkMap     `json:"links,omitempty"`
-	Status int         `json:"status"`
-	Code   int         `json:"code"`
-	Title  string      `json:"title"`
+	Status int         `json:"status,omitempty"`
+	Code   int         `json:"code,omitempty"`
+	Title  string      `json:"title,omitempty"`
 	Detail string      `json:"detail,omitempty"`
 	Source interface{} `json:"source,omitempty"` // ErrorSource
 	Meta   interface{} `json:"meta,omitempty"`
